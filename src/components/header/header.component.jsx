c6dc1244e6c17c307d2829fb597b1dc1b6dd47dc
Vincent Kapp <vincent.kapp@gmail.com>
2020-10-19 12:42:41 +0200

    Added cart icon and cart dropdown, implemented card reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     console.log(currentUser);
     return (
         <div className='header'>
@@ -21,14 +23,20 @@ const Header = ({ currentUser }) => {
                     <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                     : 
                     <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+                }
+                <CartIcon />
             </div>
+            {
+                hidden ? null:
+                <CartDropdown />
+            }    
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,4 @@
-import { userActionTypes } from './user.types'
+import UserActionTypes from './user.types'
 
 const INTIAL_STATE = {
     currentUser: null
@@ -6,7 +6,7 @@ const INTIAL_STATE = {
 
 const userReducer = (state = INTIAL_STATE, action) => {
     switch (action.type) {
-        case userActionTypes.SET_CURRENT_USER: 
+        case UserActionTypes.SET_CURRENT_USER: 
             return {
                 ...state,
                 currentUser: action.payload
