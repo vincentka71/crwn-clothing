525521fab31867ccf9b46d1ad562430489369316
Vincent Kapp <vincent.kapp@gmail.com>
2020-12-09 15:57:46 +0100

    commit from windows pc adding total quantity in the dropdown menu

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,11 +5,11 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import './cart-icon.styles.scss'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
     return (
         <div className='cart-icon' onClick={toggleCartHidden}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>   
+    <span className='item-count'>{itemCount}</span>   
         </div>
     )
 }
@@ -18,8 +18,13 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+)})
+
 export default connect(
-    null, 
+    mapStateToProps,
     mapDispatchToProps
     )(CartIcon)
 
